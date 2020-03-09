73092ad18e1dd584cfd6b00908774920122aee08
Alechj <Alechj@users.noreply.github.com>
2020-03-09 10:23:06 +0100

    Tests.. (#4)
    * Tests..
    
    * Fix a testcase

--- P8-API/P8-API-Tests/UserControllerTests.cs
+++ P8-API/P8-API-Tests/UserControllerTests.cs
@@ -6,6 +6,9 @@ using System.Text;
 using P8_API.Services;
 using P8_API.Controllers;
 using P8_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace P8_API_Tests
 {
@@ -49,6 +52,49 @@ namespace P8_API_Tests
 
             // Do Assertions
             Assert.AreEqual(result.Count, 2);
+        }
+
+        [Test]
+        public void UpdateUser_Success()
+        {
+            // Prepare objects
+            User mockUser = new User("1", "test1@gmail.com");
+            User UpdatedUser = new User("1", "testnew@gmail.com");
+            _mockDB.Setup(x => x.Get("1")).Returns(mockUser);
+            _mockDB.Setup(x => x.Update("1", UpdatedUser)).Callback((string id, User user) => mockUser = user);
+            
+            // Run Post Request
+            _controller.Put("1", UpdatedUser);
+
+            // Do Assertions
+            Assert.AreEqual(mockUser.Email, UpdatedUser.Email);
+        }
+
+        [Test]
+        public void CreateUser_Success()
+        {
+            // Prepare objects
+            User mockUser = new User("1", "test1@gmail.com");
+            _mockDB.Setup(x => x.Create(mockUser)).Returns(mockUser);
+
+            // Run Post Request
+            var res = _controller.Post(mockUser);
+
+            // Do Assertions
+            Assert.AreEqual(mockUser.Email, res.Value.Email);
+        }
+
+        [TestCase("1", ExpectedResult = StatusCodes.Status204NoContent)]
+        [TestCase("2", ExpectedResult = StatusCodes.Status404NotFound)]
+        public int? DeleteUser_Success(string id)
+        {
+            // Prepare objects
+            User mockUser = new User("1", "test1@gmail.com");
+            _mockDB.Setup(x => x.Get("1")).Returns(mockUser);
+            _mockDB.Setup(x => x.Remove(id));
+
+            // Run Post Request
+            return ((IStatusCodeActionResult)_controller.Delete(id)).StatusCode;
         }
     }
 }
