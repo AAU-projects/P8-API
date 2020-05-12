aacf9080553d1396300f8207bd37a1774460e6f8
HaZz <magnustboisen@gmail.com>
2020-05-12 14:21:09 +0200

    Update LoggingService.cs

--- P8-API/P8-API/Services/LoggingService.cs
+++ P8-API/P8-API/Services/LoggingService.cs
@@ -70,7 +70,7 @@ namespace P8_API.Services
 
                 _extractionService.SaveTrips(tripsResultList, userId);
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 return false;
             }
